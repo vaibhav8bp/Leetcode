d0a51da89ede464d830921df3cfe8c0925525ec8
Vaibhav Khanna <vaibhavkhanna@Vaibhavs-MacBook-Air.local>
2025-01-17 17:21:01 +0530

    Removed Unnecessary Check

--- src/main/java/org/example/Daily/_2025/January/_17/_2683_Neighboring_Bitwise_XOR.java
+++ src/main/java/org/example/Daily/_2025/January/_17/_2683_Neighboring_Bitwise_XOR.java
@@ -14,6 +14,6 @@ public class _2683_Neighboring_Bitwise_XOR {
     }
 
     public boolean doesValidArrayExist(int[] derived) {
-        return (XORCalculator(derived) % 2 == 0);
+        return (XORCalculator(derived) == 0);
     }
 }
