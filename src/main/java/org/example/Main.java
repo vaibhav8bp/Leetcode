969a8fdf375a75eb310be24a8c8c744424ed7bc2
Vaibhav Khanna <vaibhavkhanna@Vaibhavs-MacBook-Air.local>
2025-01-19 18:13:03 +0530

    Removed Commented Code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,9 +5,6 @@ import org.example.Weekly._433._3428_Maximum_and_Minimum_Sums_of_at_Most_Size_K_
 
 public class Main {
     public static void main(String[] args) {
-//        _2429_Minimize_XOR a = new _2429_Minimize_XOR();
-//        System.out.println(a.minimizeXor(12, 12));
-
         _3428_Maximum_and_Minimum_Sums_of_at_Most_Size_K_Subsequences a = new _3428_Maximum_and_Minimum_Sums_of_at_Most_Size_K_Subsequences();
         int[] nums = {1,1,1};
         int k = 2;
