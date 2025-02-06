ba59846814f35e047edb0447a1d40b1e85860f4a
Vaibhav Khanna <vaibhavkhanna@Vaibhavs-MacBook-Air.local>
2025-02-06 07:10:18 +0530

    06th February 2025, Daily Challenge

---
+++ src/main/java/org/example/Daily/_2025/February/_5/_1790_Check_if_One_String_Swap_Can_Make_Strings_Equal.java
@@ -0,0 +1,2 @@
+package org.example.Daily._2025.February._5;public class _1790_Check_if_One_String_Swap_Can_Make_Strings_Equal {
+}
---
+++ src/main/java/org/example/Daily/_2025/February/_6/_1726_Tuple_with_Same_Product.java
@@ -0,0 +1,2 @@
+package org.example.Daily._2025.February._6;public class _1726_Tuple_with_Same_Product {
+}
