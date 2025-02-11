736b6ae06c5dc411354e78408c0d1908e6de34c1
Vaibhav Khanna <vaibhavkhanna@Vaibhavs-MacBook-Air.local>
2025-02-11 13:07:13 +0530

    11th February 2025, Daily Challenge

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.Weekly._436._3449_Maximize_the_Minimum_Game_Score;
+
 public class Main {
     public static void main(String[] args) {
 //        _3428_Maximum_and_Minimum_Sums_of_at_Most_Size_K_Subsequences a = new _3428_Maximum_and_Minimum_Sums_of_at_Most_Size_K_Subsequences();
@@ -10,5 +12,9 @@ public class Main {
 //        Q3_Maximum_Frequency_After_Subarray_Operation a = new Q3_Maximum_Frequency_After_Subarray_Operation();
 //        int[] nums = {10, 6};
 //        System.out.println(a.maxFrequency(nums, 8));
+
+        _3449_Maximize_the_Minimum_Game_Score a = new _3449_Maximize_the_Minimum_Game_Score();
+        System.out.println(a.maxScore(new int[]{2, 4}, 3));
+        System.out.println(a.maxScore(new int[]{1, 2, 3}, 5));
     }
 }
