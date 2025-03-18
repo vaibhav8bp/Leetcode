50034c29c3f63af25a1c0d84b40a8c6a8993edf6
Vaibhav Khanna <vaibhavkhanna@Vaibhavs-MacBook-Air.local>
2025-03-18 07:09:02 +0530

    Positive Message

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,37 +1,8 @@
 package org.example;
 
-import org.example.Google._2_Add_Two_Numbers;
 
 public class Main {
     public static void main(String[] args) {
-//        _3428_Maximum_and_Minimum_Sums_of_at_Most_Size_K_Subsequences a = new _3428_Maximum_and_Minimum_Sums_of_at_Most_Size_K_Subsequences();
-//        int[] nums = {1,1,1};
-//        int k = 2;
-//        System.out.println(a.minMaxSums(nums, k));
-
-//        Q3_Maximum_Frequency_After_Subarray_Operation a = new Q3_Maximum_Frequency_After_Subarray_Operation();
-//        int[] nums = {10, 6};
-//        System.out.println(a.maxFrequency(nums, 8));
-
-        int[][] edges = {{0, 1, 2}, {1, 2, 3}, {1, 3, 5}, {1, 4, 4}, {2, 5, 6}};
-        int[] nums = {2, 1, 2, 1, 3, 1};
-
-//        int[][] edges1 = {{1, 0, 8}};
-//        int[] nums1 = {2, 2};
-//
-//        int[][] edges2 = {{2, 0, 2}, {1, 2, 10}};
-//        int[] nums2 = {1, 5, 4};
-
-//        _3425_Longest_Special_Path a = new _3425_Longest_Special_Path();
-//        System.out.println(Arrays.toString(a.longestSpecialPath(edges, nums)));
-////        System.out.println(Arrays.toString(a.longestSpecialPath(edges1, nums1)));
-////        System.out.println(Arrays.toString(a.longestSpecialPath(edges2, nums2)));
-
-//        System.out.println(Arrays.toString(("aa**").split("\\*")));
-//        System.out.println(Arrays.toString(("*aa*").split("\\*")));
-//        System.out.println(Arrays.toString(("*ab*ab").split("\\*")));
-//        System.out.println(Arrays.toString(("ab*ab*ab").split("\\*")));
-//        System.out.println(Arrays.toString(("**aa").split("\\*")));
-//        System.out.println(Arrays.toString(("**").split("\\*")));
+        System.out.println("Be Happy Vaibhav!!, You are Awesome!!, Don't ever let your inner kid die");
     }
 }
